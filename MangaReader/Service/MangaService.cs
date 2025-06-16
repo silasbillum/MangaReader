1ee5839c154d6bd0fff1338a4010b57abd47eda8
silasbillum <stilhofsvej46@gmail.com>
2025-06-16 09:49:17 +0200

    update

--- MangaReader/Service/MangaService.cs
+++ MangaReader/Service/MangaService.cs
@@ -13,7 +13,7 @@ namespace MangaReader.Service
         public async Task<List<Manga>> GetMangaListAsync(int page = 1)
         {
             //https://mangareader-c0th.onrender.com/api/mangaList?type=hot-manga&page={page}
-            var url = $"https://mangareader-c0th.onrender.com/api/mangaList?type=hot-manga&page={page}";
+            var url = $"https://mangareader-2.onrender.com/api/mangaList?type=hot-manga&page={page}";
             var result = await _httpClient.GetFromJsonAsync<MangaListResponse>(url);
             return result?.MangaList ?? new List<Manga>();
         }
